f4b01be9d0dc6f1a32c21fd09717eeb878747429
swedesjs <babaevvladislav828@gmail.com>
2021-10-17 10:34:59 +0300

    added mapIndexed function to List class

--- lib/src/utils/extension.dart
+++ lib/src/utils/extension.dart
@@ -5,8 +5,17 @@ extension IterableExtension<T> on Iterable<T> {
       yield convert(index++, element);
     }
   }
-  
 }
+
+
+extension ListExtension<T> on List<T> {
+   Iterable<R> mapIndexed<R>(R Function(int index, T element) convert) sync* {
+    var index = 0;
+    for (var element in this) {
+      yield convert(index++, element);
+    }
+  }
+} 
 
 extension StringExtension on String {
   int toInt() => int.parse(this);
